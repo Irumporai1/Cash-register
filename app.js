619c674167f5f1fb0ff4010437a3d4a11279565f
Irumporai1 <irumporai.ai@gmail.com>
2021-08-13 17:35:59 +0530

    Added input, button

---
+++ app.js
@@ -0,0 +1,5 @@
+const totalAmount = document.querySelector("#bill-amount");
+const cashGiven = document.querySelector("#cash-given");
+const chkButton = document.querySelector("#btn-check")
+const returnAmount = totalAmount.value - cashGiven.value;
+
